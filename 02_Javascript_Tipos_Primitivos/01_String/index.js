e48486a0910df6bf5b254d23e055487f30f6b53b
Luan Pinto <luan.nelmo@hotmail.com>
2024-02-22 20:42:48 -0300

    String

---
+++ 02_Javascript_Tipos_Primitivos/01_String/index.js
@@ -0,0 +1,12 @@
+console.log('"Aqui vai uma string"');
+console.log("Aqui vai uma string");
+console.log(`Aqui vai uma string`);
+
+console.log(String(2345456));
+
+/*
+    Como podemos escrever uma string
+    'Aqui vai uma string'
+    "Aqui vai uma string"
+    `Aqui vai uma string`
+*/
