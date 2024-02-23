0c968533586e5bc3811d1d2eea021f699a737af3
Luan Pinto <luan.nelmo@hotmail.com>
2024-02-23 20:13:20 -0300

    Array

---
+++ 02_Javascript_Tipos_Primitivos/05_Array/index.js
@@ -0,0 +1,6 @@
+console.log(['Javascript', 'Typescript', 'NodeJS']);
+console.log(['Javascript', 'Typescript', 'NodeJS'].length);
+
+['Javascript', 'Typescript', 'NodeJS'].forEach((res, index) => {
+    console.log(res, index);
+});
