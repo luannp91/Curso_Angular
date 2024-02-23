b265292faea775bbfb1a64c2c493a7748cfe78d3
Luan Pinto <luan.nelmo@hotmail.com>
2024-02-23 20:22:02 -0300

    Object

---
+++ 02_Javascript_Tipos_Primitivos/06_Object/index.js
@@ -0,0 +1,23 @@
+/*
+    Objeto: Tenis (Propriedade) 'cadarço', 'palmilha'
+    Objeto: Celular (Propriedade)
+    Objeto: Drone (Propriedade)
+
+    Propriedades / Atributos / Funcionalidades
+*/
+
+console.log({
+    tenis: {
+        preco: 150,
+        disponivel: true,
+        estoque: 150,
+        cadarco: {
+            cor: 'Rosa',
+            tamanho: 'G',
+        },
+        palmilha: {
+            tamanho: 43,
+            cor: 'Roxo',
+        },
+    },
+});
