8a4c9ccde3215459437e27a4dacd80d044f28796
Luan Pinto <luan.nelmo@hotmail.com>
2024-03-16 20:35:39 -0300

    Variáveis

---
+++ 04_Variaveis/01_variaveis/index.js
@@ -0,0 +1,23 @@
+/*
+    var = utilizado globalmente
+    let = gerado dentro de um escopo
+    const = gerado dentro de um escopo porém podemos armazenar dados apenas uma vez
+*/
+
+var cachorro = 'Kyara';
+console.log(cachorro);
+
+cachorro = 'Teca';
+console.log(cachorro);
+
+let lanche = 'Coxinha';
+console.log(lanche);
+
+lanche = 'Misto Quente';
+console.log(lanche);
+
+const nome = 'Johnny';
+console.log(nome);
+
+nome = 'Johnny B.';
+console.log(nome);
